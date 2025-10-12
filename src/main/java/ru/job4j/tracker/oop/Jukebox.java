26b354792e5f6f66909c8cdf93a0d92bf28e3d48
Yulia Kh <ylia.kano@gmail.com>
2025-10-13 00:05:09 +0400

    Вызов метода с аргументами

---
+++ src/main/java/ru/job4j/tracker/oop/Jukebox.java
@@ -0,0 +1,23 @@
+package ru.job4j.tracker.oop;
+
+public class Jukebox {
+    public void music(int position) {
+        switch (position) {
+            case 1:
+                System.out.println("Пусть бегут неуклюже");
+                break;
+            case 2:
+                System.out.println("Спокойной ночи");
+                break;
+            default:
+                System.out.println("Песня не найдена");
+        }
+    }
+
+    public static void main(String[] args) {
+        Jukebox jukebox = new Jukebox();
+        jukebox.music(1);
+        jukebox.music(2);
+        jukebox.music(22);
+    }
+}
