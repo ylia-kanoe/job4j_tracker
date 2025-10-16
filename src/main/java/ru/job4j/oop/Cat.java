40401793f6e3887041f7107044301a6167cc1783
Yulia Kh <ylia.kano@gmail.com>
2025-10-16 16:26:51 +0400

    Поля объекта

---
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -0,0 +1,30 @@
+package ru.job4j.oop;
+
+public class Cat {
+
+    private String food;
+    private String name;
+
+    public void show() {
+        System.out.println("There is " + this.name + "'s food: " + this.food);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public static void main(String[] args) {
+        Cat gav = new Cat();
+        gav.giveNick("Gav");
+        gav.eat("cutlet");
+        gav.show();
+        Cat black = new Cat();
+        black.giveNick("Black");
+        black.eat("fish");
+        black.show();
+    }
+}
