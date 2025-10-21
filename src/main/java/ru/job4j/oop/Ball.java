93639494102baf16afdf166ecc632f95e906bd1f
Yulia Kh <ylia.kano@gmail.com>
2025-10-21 21:23:04 +0400

    Revert "Взаимодействие объектов изменено"
    This reverts commit dbbdf78d1a3d8eb39f77c9a92b28a833e29663aa.

--- src/main/java/ru/job4j/oop/Ball.java
+++ src/main/java/ru/job4j/oop/Ball.java
@@ -2,10 +2,6 @@ package ru.job4j.oop;
 
 public class Ball {
     public void tryRun(boolean condition) {
-        if(condition) {
-            System.out.println("Колобок съеден");
-        } else {
-            System.out.println("Колобок сбежал");
-        }
+
     }
 }
