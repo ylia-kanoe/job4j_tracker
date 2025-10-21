9eb545744e1882ccb562f77796f9df662d1bf892
Yulia Kh <ylia.kano@gmail.com>
2025-10-21 21:23:19 +0400

    Взаимодействие объектов изменено

--- src/main/java/ru/job4j/oop/Ball.java
+++ src/main/java/ru/job4j/oop/Ball.java
@@ -2,6 +2,10 @@ package ru.job4j.oop;
 
 public class Ball {
     public void tryRun(boolean condition) {
-
+        if (condition) {
+            System.out.println("Колобок съеден");
+        } else {
+            System.out.println("Колобок сбежал");
+        }
     }
 }
