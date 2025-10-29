91da84db8f25085dcbd5cac7d17be14801302f0b
Yulia Kh <ylia.kano@gmail.com>
2025-10-29 10:22:59 +0400

    Перегрузить метод max для трех чисел изменено

--- src/main/java/ru/job4j/oop/Max.java
+++ src/main/java/ru/job4j/oop/Max.java
@@ -13,6 +13,4 @@ public class Max {
     public static int max(int a, int b, int c, int d) {
         return (a > b) ? ((a > c) ? ((a > d) ? a : d) : ((c > d) ? c : d)) : ((b > c) ? ((b > d) ? b : d) : ((c > d) ? c : d));
     }
-
-
 }
