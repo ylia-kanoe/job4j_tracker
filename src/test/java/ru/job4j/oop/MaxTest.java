702018be6aede976fe03f2c3152851dcbd3122b8
Yulia Kh <ylia.kano@gmail.com>
2025-10-29 10:25:45 +0400

    Перегрузить метод max для трех чисел изменено

--- src/test/java/ru/job4j/oop/MaxTest.java
+++ src/test/java/ru/job4j/oop/MaxTest.java
@@ -7,21 +7,21 @@ class MaxTest {
     @Test
     public void when5Or4ThenThisMax5() {
         int expected = 5;
-        int output = Max.max(5,4);
+        int output = Max.max(5, 4);
         assertThat(output).isEqualTo(expected);
     }
 
     @Test
     public void when5Or4Or7ThenThisMax7() {
         int expected = 7;
-        int output = Max.max(5,4, 7);
+        int output = Max.max(5, 4, 7);
         assertThat(output).isEqualTo(expected);
     }
 
     @Test
     public void when5Or12Or4Or7ThenThisMax12() {
         int expected = 12;
-        int output = Max.max(5, 12,4, 7);
+        int output = Max.max(5, 12, 4, 7);
         assertThat(output).isEqualTo(expected);
     }
 }
