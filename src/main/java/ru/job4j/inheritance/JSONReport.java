486ddc3b7e4fc9e6dbc528c448b015f1f90003b4
Yulia Kh <ylia.kano@gmail.com>
2025-10-29 11:13:47 +0400

    Переопределение метода изменено

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -1,10 +1,7 @@
 package ru.job4j.inheritance;
 
-public class JSONReport extends TextReport{
+public class JSONReport extends TextReport {
     public String generate(String name, String body) {
-        return "{" + System.lineSeparator() +
-                "\t\"name\"" + " : " + "\"" + name + "\"" + "," + System.lineSeparator() +
-                "\t\"body\"" + " : " + "\"" + body + "\"" + System.lineSeparator() +
-                "}";
+        return "{" + System.lineSeparator() + "\t\"name\"" + " : " + "\"" + name + "\"" + "," + System.lineSeparator() + "\t\"body\"" + " : " + "\"" + body + "\"" + System.lineSeparator() + "}";
     }
 }
