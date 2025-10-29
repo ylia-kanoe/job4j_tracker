33413189b151e4fd326050def460ed50d3980c09
Yulia Kh <ylia.kano@gmail.com>
2025-10-29 09:51:34 +0400

    Рефакторинг - Площадь треугольника изменено

--- src/main/java/ru/job4j/oop/Triangle.java
+++ src/main/java/ru/job4j/oop/Triangle.java
@@ -16,10 +16,8 @@ public class Triangle {
     }
 
     public boolean exist(double ab, double ac, double bc) {
-        if ((ab + ac > bc) && (ab + bc > ac) && (bc + ac > ab)) {
-            return true;
-        }
-        return false;
+        return (ab + ac > bc) && (ab + bc > ac) && (bc + ac > ab);
+
     }
 
     public double area() {
