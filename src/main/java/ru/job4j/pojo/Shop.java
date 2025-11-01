3d2ee5d5b6633798d0a6c6e5ab6c7fcac1f78fcd
Yulia Kh <ylia.kano@gmail.com>
2025-11-01 13:07:46 +0400

    Массив с пустыми ячейками

---
+++ src/main/java/ru/job4j/pojo/Shop.java
@@ -0,0 +1,21 @@
+package ru.job4j.pojo;
+
+public class Shop {
+
+    public static int indexOfNull(Product[] products) {
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            if (product == null) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Product[] products = new Product[5];
+        products[0] = new Product("Milk", 10);
+        products[1] = new Product("Bread", 4);
+        products[2] = new Product("Egg", 19);
+    }
+}
