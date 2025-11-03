4ae63fa577ab7d77d61a764aef6820422383643a
Yulia Kh <ylia.kano@gmail.com>
2025-11-03 15:15:39 +0400

    Массивы и модели изменено

--- src/main/java/ru/job4j/pojo/Book.java
+++ src/main/java/ru/job4j/pojo/Book.java
@@ -1,31 +1,27 @@
 package ru.job4j.pojo;
 
 public class Book {
-    public static void main(String[] args) {
-        Library bookOne = new Library("The Complete Coding Interview Guide in Java", 788);
-        Library bookTwo = new Library("Структуры данных и алгоритмы в Java", 704);
-        Library bookThree = new Library("Spring Boot по-быстрому", 352);
-        Library bookFour = new Library("Clean code", 464);
-        Library[] librarys = new Library[4];
-        librarys[0] = bookOne;
-        librarys[1] = bookTwo;
-        librarys[2] = bookThree;
-        librarys[3] = bookFour;
-        for (int index = 0; index < librarys.length; index++) {
-            Library library = librarys[index];
-            System.out.println(library.getName() + " - " + library.getCount());
-        }
-        librarys[0] = bookFour;
-        librarys[3] = bookOne;
-        for (int index = 0; index < librarys.length; index++) {
-            Library library = librarys[index];
-            System.out.println(library.getName() + " - " + library.getCount());
-        }
-        for (int index = 0; index < librarys.length; index++) {
-            Library library = librarys[index];
-            if (library.getName() == "Clean code") {
-                System.out.println(library.getName() + " - " + library.getCount());
-            }
-        }
+    private String name;
+    private int count;
+
+    public Book(String name, int count) {
+        this.name = name;
+        this.count = count;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
     }
 }
--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -1,27 +1,31 @@
 package ru.job4j.pojo;
 
 public class Library {
-    private String name;
-    private int count;
-
-    public Library(String name, int count) {
-        this.name = name;
-        this.count = count;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getCount() {
-        return count;
-    }
-
-    public void setCount(int count) {
-        this.count = count;
+    public static void main(String[] args) {
+        Book bookOne = new Book("The Complete Coding Interview Guide in Java", 788);
+        Book bookTwo = new Book("Структуры данных и алгоритмы в Java", 704);
+        Book bookThree = new Book("Spring Boot по-быстрому", 352);
+        Book bookFour = new Book("Clean code", 464);
+        Book[] librarys = new Book[4];
+        librarys[0] = bookOne;
+        librarys[1] = bookTwo;
+        librarys[2] = bookThree;
+        librarys[3] = bookFour;
+        for (int index = 0; index < librarys.length; index++) {
+            Book library = librarys[index];
+            System.out.println(library.getName() + " - " + library.getCount());
+        }
+        librarys[0] = bookFour;
+        librarys[3] = bookOne;
+        for (int index = 0; index < librarys.length; index++) {
+            Book library = librarys[index];
+            System.out.println(library.getName() + " - " + library.getCount());
+        }
+        for (int index = 0; index < librarys.length; index++) {
+            Book library = librarys[index];
+            if (library.getName().equals("Clean code")) {
+                System.out.println(library.getName() + " - " + library.getCount());
+            }
+        }
     }
 }
